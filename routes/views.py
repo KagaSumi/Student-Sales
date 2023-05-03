6b082101005a3db284e658df71ca20a7cbd01ff1
Quinten Leung <wleung105@my.bcit.ca>
2023-05-03 11:25:00 -0700

    Create listing api endpoint

--- routes/views.py
+++ routes/views.py
@@ -13,3 +13,28 @@ def home():
 @login_required
 def profile():
     return render_template('profile.html', user=current_user)
+
+@views.route('/create_listing', methods=['GET', 'POST'])
+@login_required
+def create_listing():
+    errors = {}
+    if request.method == 'POST':
+        title = request.form.get('title')
+        description = request.form.get('description')
+        if not description:
+            description = 'None'
+        price = request.form.get('price')
+        if not price:
+            price = 0
+
+        listing = Listing(title=title, description=description,
+                          price=price, user_id=current_user.id)
+        db.session.add(listing)
+
+        if not errors:
+            db.session.commit()
+            print(listing)
+            flash('New Listing Created!', 'success')
+            return redirect(url_for('views.profile', listing=listing))
+
+    return render_template('create_listing.html', user=current_user, errors=errors)
