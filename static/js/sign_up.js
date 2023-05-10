5c085f93372200f3d6ea0b99b33d9dbae835adba
Stanley <sle23@my.bcit.ca>
2023-05-10 13:40:29 -0700

    grid css for signup.html,symbols required for password,  and also took into consideration that phone numbers are now restricted to only integers

--- static/js/sign_up.js
+++ static/js/sign_up.js
@@ -1,17 +1,18 @@
 const email = document.getElementById('email');
 const first_name = document.getElementById('firstName');
 const last_name = document.getElementById('lastName');
-const phone_number = document.getElementById('phonenumber');
+const phone_number = document.getElementById('phone_number');
 const password = document.getElementById('password');
 const confirm_password = document.getElementById('conf_password');
 const submitBTN = document.getElementById('submit_button');
 
+const pattern = /^[0-9]{3}-?[0-9]{3}-?[0-9]{4}$/;
 const symbols = ['$','!','@','#','%','^','&','*','_','+','=','-','`','~'];
 /* Error Handling For Input Fields*/
 const email_error = document.getElementById('email_error');
 const firstName_error = document.getElementById('firstName_error');
 const lastName_error = document.getElementById('lastName_error');
-const phonenumber_error = document.getElementById('phonenumber_error');
+const phone_number_error = document.getElementById('phone_number_error');
 const password_error = document.getElementById('password_error');
 const conf_password_error = document.getElementById('conf_password_error');
 
@@ -43,10 +44,10 @@ const verify_email = (event) => {
 
   if (!isValidEmail) {
     email_error.textContent = "Please provide a valid email address.";
-    email_error.classList.add("error");
+    email_error.classList.add("error-message");
   } else {
     email_error.textContent = "";
-    email_error.classList.remove("error");
+    email_error.classList.remove("error-message");
   }
 };
 
@@ -57,10 +58,10 @@ const verify_firstName = (event) => {
 
   if (!isValidFirstName) {
     firstName_error.textContent = "Please provide a valid first name.";
-    firstName_error.classList.add("error");
+    firstName_error.classList.add("error-message");
   } else {
     firstName_error.textContent = "";
-    firstName_error.classList.remove("error");
+    firstName_error.classList.remove("error-message");
   }
 };
 
@@ -71,12 +72,29 @@ const verify_lastName = (event) => {
 
   if (!isValidLastName) {
     lastName_error.textContent = "Please provide a valid last name.";
-    lastName_error.classList.add("error");
+    lastName_error.classList.add("error-message");
   } else {
     lastName_error.textContent = "";
-    lastName_error.classList.remove("error");
+    lastName_error.classList.remove("error-message");
   }
 };
+
+
+const verify_phone_number = (event) => {
+  const phone_number = document.getElementById('phone_number');
+  const phone_number_error = document.getElementById('phone_number_error');
+  const pattern = /^[0-9]{3}-?[0-9]{3}-?[0-9]{4}$/; // 10-digit phone number with optional hyphens
+  const isValidPhoneNumber = pattern.test(phone_number.value) || phone_number.value.length == 0;
+  
+  if (!isValidPhoneNumber) {
+    phone_number_error.textContent = "Please enter a valid 10-digit phone number.";
+    phone_number_error.classList.add("error-message");
+  } else {
+    phone_number_error.textContent = "";
+    phone_number_error.classList.remove("error-message");
+  }
+};
+
 
 const verify_password = (event) => {
   const password = document.getElementById('password');
@@ -85,14 +103,14 @@ const verify_password = (event) => {
 
   if (password.value.length < 4) {
     password_error.textContent = "Password is too short.";
-    password_error.classList.add("error");
+    password_error.classList.add("error-message");
   } else if (!hasSymbol) {
     password_error.textContent = "Password must contain a symbol. (e.g. $, !, @, #, %, ^, &, *, _, +, =, -, `, ~)";
-    password_error.classList.add("error");
+    password_error.classList.add("error-message");
   }
    else {
     password_error.textContent = "";
-    password_error.classList.remove("error");
+    password_error.classList.remove("error-message");
   }
 };
 
@@ -103,10 +121,10 @@ const verify_confirm_password = (event) => {
 
   if (!isValidpassword) {
     conf_password_error.textContent = "Passwords do not match.";
-    conf_password_error.classList.add("error");
+    conf_password_error.classList.add("error-message");
   } else {
     conf_password_error.textContent = "";
-    conf_password_error.classList.remove("error");
+    conf_password_error.classList.remove("error-message");
   }
 };
 
@@ -152,5 +170,20 @@ for (element of [email, first_name, last_name, password, confirm_password, phone
 email.addEventListener('input', verify_email);
 first_name.addEventListener('input', verify_firstName);
 last_name.addEventListener('input', verify_lastName);
+phone_number.addEventListener('input', verify_phone_number);
 password.addEventListener('input', verify_password);
 confirm_password.addEventListener('input', verify_confirm_password);
+
+phone_number.addEventListener('keypress', function(event) {
+  // Prevent non-numeric input
+  if (isNaN(parseInt(event.key))) {
+    event.preventDefault();
+    return;
+  }
+  
+  // Limit input to 10 digits
+  const currentValue = phone_number.value.replace(/[^0-9]/g, '');
+  if (currentValue.length >= 10) {
+    event.preventDefault();
+  }
+});
