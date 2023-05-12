5113a9e1cc32b6c644ca2279d3664d3a9201b642
Quinten Leung <wleung105@my.bcit.ca>
2023-05-11 18:55:25 -0700

    removed password changes and fixed padding issue on alert message

--- static/js/account.js
+++ static/js/account.js
@@ -5,14 +5,6 @@ const delete_button = document.getElementById("delete_button");
 
 const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '='];
 
-const old_password = document.getElementById("old_password");
-const old_password_error = document.getElementById('password_error');
-
-const new_password = document.getElementById("new_password");
-const new_password_error = document.getElementById('new_password_error');
-
-const confirm_password = document.getElementById("confirm_password");
-const confirm_password_error = document.getElementById("confirm_password_error");
 
 const phone_number = document.getElementById("phone_number");
 const phone_number_error = document.getElementById("phone_number_error");
@@ -33,13 +25,11 @@ const verify_fields = (event) =>{
 
 const update_request = () => {
   // Check whether the new password meets the requirements
-  verify_new_password();
 
   // Check whether the confirmed password matches the new password
-  verify_confirm_password();
 
   // If there is no error message, send a request to update the profile
-  if (!new_password_error.textContent && !confirm_password_error.textContent) {
+  if (false) {
       let payload = { first_name: first_name.value, last_name: last_name.value, phone_number: phone_number.value };
       fetch('/update_profile', {
           method: 'PUT',
@@ -67,37 +57,9 @@ for (element of [first_name,last_name]){
     element.addEventListener('input', verify_fields);
 }
 
-const verify_new_password = (event) =>{
-  const new_password = document.getElementById('new_password');
-  const new_password_error = document.getElementById('new_password_error');
-  const hasSymbol = symbols.some((symbol) => new_password.value.includes(symbol));
 
-  if (new_password.value.length < 4) {
-    new_password_error.textContent = "Password is too short.";
-    new_password_error.classList.add("error-message");
-  } else if (!hasSymbol) {
-    new_password_error.textContent = "Password must contain a symbol.";
-    new_password_error.classList.add("error-message");
-  }
-   else {
-    new_password_error.textContent = "";
-    new_password_error.classList.remove("error-message");
-  }
-};
 
-const verify_confirm_password = (event) => {
-  const confirm_password = document.getElementById('confirm_password');
-  const confirm_password_error = document.getElementById('confirm_password_error');
-  const isValidpassword = confirm_password.value === new_password.value;
 
-  if (!isValidpassword) {
-    confirm_password_error.textContent = "Passwords do not match.";
-    confirm_password_error.classList.add("error-message");
-  } else {
-    confirm_password_error.textContent = "";
-    confirm_password_error.classList.remove("error-message");
-  }
-};
 
 const verify_phone_number = (event) => {
   const phone_number = document.getElementById('phone_number');
@@ -115,8 +77,6 @@ const verify_phone_number = (event) => {
 };
 
 /* Event Listeners */
-new_password.addEventListener('input', verify_new_password);
-confirm_password.addEventListener('input', verify_confirm_password);
 phone_number.addEventListener('input', verify_phone_number);
 
 phone_number.addEventListener('keypress', function(event) {
