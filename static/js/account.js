bb500b8729806a7862f03800a0794daf3ecabd4e
Quinten Leung <wleung105@my.bcit.ca>
2023-05-11 19:00:02 -0700

    fixed logic

--- static/js/account.js
+++ static/js/account.js
@@ -15,7 +15,7 @@ const delete_user = () => {
 }
 
 const verify_fields = (event) =>{
-    if (first_name.value.length == 0 || last_name.value.length == 0) {
+    if (first_name.value.length == 0 || last_name.value.length == 0 || !(phone_number.value.length == 0 || phone_number.value.length == 10)) {
         update_button.classList.add("disabled");
     }
     else{
@@ -24,36 +24,30 @@ const verify_fields = (event) =>{
 }
 
 const update_request = () => {
-  // Check whether the new password meets the requirements
+    let payload = { first_name: first_name.value, last_name: last_name.value, phone_number: phone_number.value };
+    fetch('/update_profile', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    })
+    .then((response) => {
+        return Promise.all([response.json(), response.status]);
+    })
+    .then(([json, status]) => {
+        let message = json.message;
+        localStorage.setItem('message', message);
+        window.location.href = '/account';
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
 
-  // Check whether the confirmed password matches the new password
-
-  // If there is no error message, send a request to update the profile
-  if (false) {
-      let payload = { first_name: first_name.value, last_name: last_name.value, phone_number: phone_number.value };
-      fetch('/update_profile', {
-          method: 'PUT',
-          headers: {
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(payload),
-      })
-      .then((response) => {
-          return Promise.all([response.json(), response.status]);
-      })
-      .then(([json, status]) => {
-          let message = json.message;
-          localStorage.setItem('message', message);
-          window.location.href = '/account';
-      })
-      .catch((error) => {
-          console.log(error);
-      });
-  }
-};
 
 update_button.addEventListener("click",update_request);
-for (element of [first_name,last_name]){
+for (element of [first_name,last_name,phone_number]){
     element.addEventListener('input', verify_fields);
 }
 
